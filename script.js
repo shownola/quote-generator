f257954b1db4a11cc0f57b7cdefb02ce1573921b
shownola <swasieko@gmail.com>
2020-09-15 22:29:44 -0700

    manipulate the dom to show quote and ability to tweet quote to twitter

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-// Get Quote From api
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
+// Get Quote From api
 async function getQuote(){
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    if(data.quoteQuthor === ''){
+      author.Text.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    if(data.quoteText.length > 120){
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.innerText = data.quoteText;
   } catch(error){
     getQuote();
     console.log('whoops, no quote', error);
   }
 }
+
+// Tweet Quote
+function tweetQuote(){
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
