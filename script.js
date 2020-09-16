59746ef60db87414742c1f4a6d95078b2fa09956
shownola <swasieko@gmail.com>
2020-09-16 10:04:41 -0700

    Refactor spinner code

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 const loader = document.getElementById('loader');
 
-// Show Loading
-
-function loading(){
+function showLoadingSpinner(){
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete(){
+function removeLoadingSpinner(){
   if(!loader.hidden){
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -23,7 +20,7 @@ function complete(){
 
 // Get Quote From api
 async function getQuote(){
-  loading();
+  showLoadingSpinner();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
@@ -40,9 +37,8 @@ async function getQuote(){
       quoteText.classList.remove('long-quote');
     }
 
-    quoteText.innerText = data.quoteText;
-    // Stop Loader & Show Quote
-    complete();
+    quoteText.innerText = data.quoteText;    
+    removeLoadingSpinner();
   } catch(error){
     getQuote();
     console.log('whoops, no quote', error);
